7440a1c9d09ce6363941914855083bac540a4530
vrebierr <vrebierr@student.42.fr>
2014-10-17 23:25:22 +0200

    add advertiser route

--- client/app/advertiser/advertiser.js
+++ client/app/advertiser/advertiser.js
@@ -1,11 +1,16 @@
 'use strict';
 
 angular.module('theHomePassApp')
-  .config(function ($stateProvider) {
-    $stateProvider
-      .state('advertiser', {
-        url: '/advertiser',
-        templateUrl: 'app/advertiser/advertiser.html',
-        controller: 'AdvertiserCtrl'
-      });
-  });
+	.config(function ($stateProvider) {
+		$stateProvider
+			.state('advertiser', {
+				url: '/advertiser',
+				templateUrl: 'app/advertiser/advertiser.html',
+				controller: 'AdvertiserCtrl',
+				resolve: {
+					ads: function (Restangular) {
+						return Restangular.all('ads').getList();
+					}
+				}
+			});
+	});
