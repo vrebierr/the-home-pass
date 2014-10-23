54072126be22fa3fd684ece221d02854ed74bf16
Valentin REBIERRE <vrebierr@student.42.fr>
2014-10-23 04:12:03 +0200

    prez

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -6,8 +6,18 @@ angular.module('theHomePassApp')
         $scope.ads = ads;
         $scope.categories = categories;
         $scope.selected = {};
-    	$scope.map = L.map('map').setView([48.89670230000001, 2.3183781999999997], 13);
-        L.tileLayer('http://{s}.tiles.mapbox.com/v3/examples.map-i875mjb7/{z}/{x}/{y}.png').addTo($scope.map);
+    	$scope.open = L.map('open').setView([48.89670230000001, 2.3183781999999997], 13);
+        L.tileLayer('http://{s}.tiles.mapbox.com/v3/examples.map-i875mjb7/{z}/{x}/{y}.png').addTo($scope.open);
+
+        $scope.initialize = function () {
+            var mapOptions = {
+                zoom: 12,
+                center: new google.maps.LatLng(48.89670230000001, 2.3183781999999997)
+            };
+            $scope.map = new google.maps.Map(document.getElementById('map'), mapOptions);
+        };
+
+        google.maps.event.addDomListener(window, 'load', $scope.initialize());
 
         $scope.clickMarker = function (item) {
             $scope.selected = item;
