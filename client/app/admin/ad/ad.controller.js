f925b203d51baf952b8ea7dff4458041b5c7a75d
Valentin REBIERRE <vrebierr@student.42.fr>
2014-10-26 10:09:58 +0100

    change

--- client/app/admin/ad/ad.controller.js
+++ client/app/admin/ad/ad.controller.js
@@ -4,5 +4,7 @@ angular.module('theHomePassApp')
     .controller('AdCtrl', function ($scope, ads) {
         $scope.ads = ads;
 
-
+        $scope.validate = function (ad) {
+            
+        };
     });
