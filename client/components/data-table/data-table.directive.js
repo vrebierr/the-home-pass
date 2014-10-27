79e197e624c1a4253beead790f2d35cac2917434
Valentin REBIERRE <vrebierr@student.42.fr>
2014-10-27 16:40:15 +0100

    table ok

--- client/components/data-table/data-table.directive.js
+++ client/components/data-table/data-table.directive.js
@@ -1,27 +1,30 @@
 'use strict';
 
-  angular.module('theHomePassApp')
-      .directive('ngTable', function ($filter) {
-          return {
-              templateUrl: 'components/data-table/data-table.html',
-              restrict: 'EA',
-              scope: {
-                  model: '=',
-                  cols: '='
-              },
-              link: function (scope, element, attrs) {
-                  scope.items = scope.model;
-                  scope.current = scope.items.slice(0, 10);
-                  scope.currentStart = 1;
-                  scope.currentEnd = 10;
+    angular.module('theHomePassApp')
+        .directive('ngTable', function ($filter) {
+            return {
+                templateUrl: 'components/data-table/data-table.html',
+                restrict: 'EA',
+                scope: {
+                    model: '=',
+                    cols: '='
+                },
+                link: function (scope, element, attrs) {
+                    scope.items = scope.model;
+                    scope.current = scope.items.slice(0, 10);
+                    scope.currentStart = 1;
+                    scope.currentEnd = 10;
 
-                  scope.pageChanged = function () {
-                      scope.current = scope.items.slice(10 * (scope.currentPage - 1), 10 * (scope.currentPage - 1) + 10);
-                      scope.currentStart = 10 * (scope.currentPage - 1) + 1;
-                      scope.currentEnd = 10 * (scope.currentPage - 1) + 10;
-                  };
+                    scope.pageChanged = function () {
+                        scope.current = scope.items.slice(10 * (scope.currentPage - 1), 10 * (scope.currentPage - 1) + 10);
+                        scope.currentStart = 10 * (scope.currentPage - 1) + 1;
+                        scope.currentEnd = 10 * (scope.currentPage - 1) + 10;
+                    };
 
-                  console.log($filter);
+                    var filter = $filter('filter');
+                    scope.$watch('search', function () {
+                        scope.items = filter(scope.model, scope.search);
+                    });
               }
           };
-  });
+    });
