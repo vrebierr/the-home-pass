4612472d8a59b4ba2295effbc8f9c9aa3a2bbb4f
Valentin REBIERRE <vrebierr@student.42.fr>
2014-10-27 15:47:21 +0100

    data table

--- client/components/data-table/data-table.directive.js
+++ client/components/data-table/data-table.directive.js
@@ -1,20 +1,27 @@
 'use strict';
 
   angular.module('theHomePassApp')
-      .directive('ngTable', function ($rootScope) {
+      .directive('ngTable', function ($filter) {
           return {
               templateUrl: 'components/data-table/data-table.html',
               restrict: 'EA',
               scope: {
                   model: '=',
+                  cols: '='
               },
               link: function (scope, element, attrs) {
-                  scope.items = scope.model.slice(0, 10);
+                  scope.items = scope.model;
+                  scope.current = scope.items.slice(0, 10);
+                  scope.currentStart = 1;
+                  scope.currentEnd = 10;
 
                   scope.pageChanged = function () {
-                      scope.items = scope.model.slice(10 * (scope.currentPage - 1), 10 * (scope.currentPage - 1) + 10);
-                      console.log(10 * (scope.currentPage - 1));
+                      scope.current = scope.items.slice(10 * (scope.currentPage - 1), 10 * (scope.currentPage - 1) + 10);
+                      scope.currentStart = 10 * (scope.currentPage - 1) + 1;
+                      scope.currentEnd = 10 * (scope.currentPage - 1) + 10;
                   };
+
+                  console.log($filter);
               }
           };
   });
