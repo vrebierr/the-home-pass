13105cac19c8ac8b84dbb52e4d3c45007a9562cc
Valentin REBIERRE <vrebierr@student.42.fr>
2014-11-04 10:03:01 +0100

    range in main

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -1,11 +1,12 @@
 'use strict';
 
 angular.module('theHomePassApp')
-    .controller('MainCtrl', ['$scope', 'pos', 'ads', 'categories', 'GoogleMapApi'.ns(), function ($scope, pos, ads, categories, GoogleMapApi) {
+    .controller('MainCtrl', ['$scope', 'pos', 'ads', 'categories', 'Auth', 'GoogleMapApi'.ns(), function ($scope, pos, ads, categories, Auth, GoogleMapApi) {
     	$scope.pos = pos;
         $scope.ads = ads;
         $scope.categories = categories;
         $scope.selected = {};
+        $scope.range = 0;
 
         GoogleMapApi.then(function (maps) {
             $scope.map = {
@@ -21,5 +22,45 @@ angular.module('theHomePassApp')
                     $scope.ads = _.where(ads, {pos: [model._id]});
                 }
             };
+        });
+
+        var distance;
+        $('#range').ionRangeSlider({
+            min: 2,
+            max: 50,
+            type: 'single',
+            step: 2,
+            postfix: 'km',
+            onChange: function (item) {
+                $scope.range = item.fromNumber;
+                $scope.circle = {
+                    stroke: {
+                        color: '#ff',
+                        weight: 2,
+                        opacity: 0.5
+                    },
+                    fill: {
+                        color: '#ff',
+                        opacity: 0.25
+                    },
+                    center: {
+                        latitude: Auth.getCurrentUser().from.latitude,
+                        longitude: Auth.getCurrentUser().from.longitude
+                    }
+                };
+
+                $scope.pos = _.filter(pos, function (item) {
+                    distance = geolib.getDistance({
+                        latitude: item.latitude,
+                        longitude: item.longitude
+                    }, {
+                        latitude: Auth.getCurrentUser().from.latitude,
+                        longitude: Auth.getCurrentUser().from.longitude
+                    });
+
+                    return $scope.range * 1000 - distance >= 0;
+                });
+                $scope.$apply();
+            }
         });
     }]);
