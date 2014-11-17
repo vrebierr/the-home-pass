f78b5185506b03110ac32dac889345ab2e3f1b7c
Valentin REBIERRE <vrebierr@student.42.fr>
2014-11-17 21:33:38 +0100

    update tests

--- server/api/ad/ad.spec.js
+++ server/api/ad/ad.spec.js
@@ -6,15 +6,40 @@ var request = require('supertest');
 
 describe('GET /api/ads', function() {
 
-  it('should respond with JSON array', function(done) {
-    request(app)
-      .get('/api/ads')
-      .expect(200)
-      .expect('Content-Type', /json/)
-      .end(function(err, res) {
-        if (err) return done(err);
-        res.body.should.be.instanceof(Array);
-        done();
-      });
-  });
+    var user = new User({
+        provider: 'local',
+        name: 'User',
+        email: 'test@test.com',
+        password: 'password',
+        role: 'advertiser'
+    });
+
+    it("should authenticate user if password is valid", function() {
+        return user.authenticate('password').should.be.true;
+    });
+
+
+    it('should respond with JSON array', function(done) {
+        request(app)
+            .get('/api/ads')
+            .expect(200)
+            .expect('Content-Type', /json/)
+            .end(function(err, res) {
+            if (err) return done(err);
+            res.body.should.be.instanceof(Array);
+            done();
+        });
+    });
+
+    it('should respond with JSON array', function(done) {
+        request(app)
+            .pos('/api/ads', {test: 'test'})
+            .expect(500)
+            .expect('Content-Type', /json/)
+            .end(function(err, res) {
+            if (err) return done(err);
+            res.body.should.be.instanceof(Array);
+            done();
+        });
+    });
 });
