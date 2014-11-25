c61250c5cc116d6221367a1460206f16e62cfb7f
Valentin REBIERRE <vrebierr@student.42.fr>
2014-11-25 22:45:00 +0100

    work ad

--- client/app/advertiser/advertiser.js
+++ client/app/advertiser/advertiser.js
@@ -7,15 +7,5 @@ angular.module('theHomePassApp')
                 url: '/advertiser',
                 templateUrl: 'app/advertiser/advertiser.html',
                 controller: 'AdvertiserCtrl'
-            })
-                .state('signupAdvertiser', {
-                url: '/advertiser/signup',
-                templateUrl: 'app/advertiser/account/signup/signup.html',
-                controller: 'SignupAdvertiserCtrl'
-            })
-            .state('loginAdvertiser', {
-                url: '/advertiser/login',
-                templateUrl: 'app/advertiser/account/login/login.html',
-                controller: 'LoginAdvertiserCtrl'
             });
     });
