736159a2c9dd9106e5527c0b6746dd6cc2a34793
Valentin REBIERRE <vrebierr@student.42.fr>
2014-12-09 19:49:34 +0100

    s

--- client/app/app.js
+++ client/app/app.js
@@ -1,24 +1,24 @@
 'use strict';
 
 angular.module('theHomePassApp', [
-  'ngCookies',
-  'ngResource',
-  'ngSanitize',
-  'btford.socket-io',
-  'ui.router',
-  'restangular',
-  'angularFileUpload',
-  'selectize-ng',
-  'uuid4',
-  'uiGmapgoogle-maps',
-  'ngMorph',
-  'mm.foundation',
-  'infinite-scroll',
-  'LocalStorageModule',
-  'highcharts-ng',
-  'angularMoment',
-  'ngCkeditor',
-  'pascalprecht.translate'
+    'ngCookies',
+    'ngResource',
+    'ngSanitize',
+    'btford.socket-io',
+    'ui.router',
+    'restangular',
+    'angularFileUpload',
+    'selectize-ng',
+    'uuid4',
+    'uiGmapgoogle-maps',
+    'ngMorph',
+    'mm.foundation',
+    'infinite-scroll',
+    'LocalStorageModule',
+    'highcharts-ng',
+    'angularMoment',
+    'ngCkeditor',
+    'pascalprecht.translate'
 ])
     .config(function ($stateProvider, $urlRouterProvider, $locationProvider, $httpProvider, RestangularProvider, uiGmapGoogleMapApiProvider, $translateProvider) {
         $urlRouterProvider
@@ -49,39 +49,39 @@ angular.module('theHomePassApp', [
         $translateProvider.preferredLanguage('fr');
     })
 
-  .factory('authInterceptor', function ($rootScope, $q, $cookieStore, $location) {
-    return {
-      // Add authorization token to headers
-      request: function (config) {
-        config.headers = config.headers || {};
-        if ($cookieStore.get('token')) {
-          config.headers.Authorization = 'Bearer ' + $cookieStore.get('token');
-        }
-        return config;
-      },
+    .factory('authInterceptor', function ($rootScope, $q, $cookieStore, $location) {
+        return {
+            // Add authorization token to headers
+            request: function (config) {
+                config.headers = config.headers || {};
+                if ($cookieStore.get('token')) {
+                    config.headers.Authorization = 'Bearer ' + $cookieStore.get('token');
+                }
+                return config;
+            },
 
-      // Intercept 401s and redirect you to login
-      responseError: function(response) {
-        if(response.status === 401) {
-          $location.path('/login');
-          // remove any stale tokens
-          $cookieStore.remove('token');
-          return $q.reject(response);
-        }
-        else {
-          return $q.reject(response);
-        }
-      }
-    };
-  })
+            // Intercept 401s and redirect you to login
+            responseError: function(response) {
+                if(response.status === 401) {
+                    $location.path('/login');
+                    // remove any stale tokens
+                    $cookieStore.remove('token');
+                    return $q.reject(response);
+                }
+                else {
+                    return $q.reject(response);
+                }
+            }
+        };
+    })
 
-  .run(function ($rootScope, $location, Auth) {
-    // Redirect to login if route requires auth and you're not logged in
-    $rootScope.$on('$stateChangeStart', function (event, next) {
-      Auth.isLoggedInAsync(function(loggedIn) {
-        if (next.authenticate && !loggedIn) {
-          $location.path('/login');
-        }
-      });
+    .run(function ($rootScope, $location, Auth) {
+        // Redirect to login if route requires auth and you're not logged in
+        $rootScope.$on('$stateChangeStart', function (event, next) {
+            Auth.isLoggedInAsync(function(loggedIn) {
+                if (next.authenticate && !loggedIn) {
+                    $location.path('/login');
+                }
+            });
+        });
     });
-  });
