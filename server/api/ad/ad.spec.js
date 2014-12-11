a7b3e3c5d08ec36c8d1186b967caa6492bdfe0e6
Valentin <vrebierr@student.42.fr>
2014-12-11 12:56:40 +0100

    update tests

--- server/api/ad/ad.spec.js
+++ server/api/ad/ad.spec.js
@@ -5,42 +5,42 @@ var app = require('../../app');
 var request = require('supertest');
 var User = require('../user/user.model');
 
-describe('GET /api/ads', function() {
+var user = new User({
+    provider: 'local',
+    name: 'User',
+    email: 'test@test.com',
+    password: 'password',
+    role: 'advertiser'
+});
 
-    var user = new User({
-        provider: 'local',
-        name: 'User',
-        email: 'test@test.com',
-        password: 'password',
-        role: 'advertiser'
+describe('POST /api/ads', function () {
+    it('should create a new ad and return it', function (done) {
+        var item = {
+            type: 'test'
+        };
+
+        request(app)
+            .post('/api/ads', item)
+            .expect(201)
+            .expect('Content-Type', /json/)
+            .end(function (err, res) {
+                if (err) {return done(err);}
+                res.body.should.be.instanceof(Object).have.property('author');
+                done();
+            });
     });
+});
 
-    it("should authenticate user if password is valid", function() {
-        return user.authenticate('password').should.be.true;
-    });
-
-
+describe('GET /api/ads', function () {
     it('should respond with JSON array', function(done) {
         request(app)
             .get('/api/ads')
             .expect(200)
             .expect('Content-Type', /json/)
             .end(function(err, res) {
-            if (err) return done(err);
-            res.body.should.be.instanceof(Array);
-            done();
-        });
-    });
-
-    it('should respond with JSON array', function(done) {
-        request(app)
-            .pos('/api/ads', {test: 'test'})
-            .expect(500)
-            .expect('Content-Type', /json/)
-            .end(function(err, res) {
-            if (err) return done(err);
-            res.body.should.be.instanceof(Array);
-            done();
-        });
+                if (err) return done(err);
+                res.body.should.be.instanceof(Array);
+                done();
+            });
     });
 });
--- server/api/comment/comment.spec.js
+++ server/api/comment/comment.spec.js
@@ -6,15 +6,15 @@ var request = require('supertest');
 
 describe('GET /api/comments', function() {
 
-  it('should respond with JSON array', function(done) {
-    request(app)
-      .get('/api/comments')
-      .expect(200)
-      .expect('Content-Type', /json/)
-      .end(function(err, res) {
-        if (err) return done(err);
-        res.body.should.be.instanceof(Array);
-        done();
-      });
-  });
+    it('should respond with JSON array', function(done) {
+        request(app)
+            .get('/api/comments')
+            .expect(200)
+            .expect('Content-Type', /json/)
+            .end(function(err, res) {
+                if (err) return done(err);
+                res.body.should.be.instanceof(Array);
+                done();
+            });
+    });
 });
